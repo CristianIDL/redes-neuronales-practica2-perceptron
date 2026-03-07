fb8d9f97d7854eb88b64b8187c7a679f398e27aa
CristianIDL <delgado.lucero.cristian@gmail.com>
2026-03-07 17:20:00 -0600

    feat(graficar): mostrar frontera de decisión

--- main.py
+++ main.py
@@ -13,16 +13,36 @@ def imprimir_actual(w,b,a):
     print("b =", b)
     print("a =", a)
     
+def frontera_decision(ax, w, b, x_range=[-0.5, 1.5]):
+    '''Dibuja la frontera de decisión del perceptrón para visualizarla en el gráfico.'''
+
+    x1 = np.linspace(x_range[0], x_range[1], 100)  # Generamos una línea hecha de 100 puntos
+
+    if w[1] != 0:  # Evitamos división por cero
+        x2 = -(w[0] * x1 + b) / w[1]  # Calculamos x2 usando la ecuación de la recta
+        ax.plot(x1, x2, 'g-', linewidth=2, label='Frontera de decisión')
+
+
 def visualizar_iteracion(x,w,b,t,epoca,iteracion,titulo=""):
     '''Visualiza la iteración actual del perceptrón.'''
     fig, ax = plt.subplots(figsize=(6,6))
+
+    # Dibujamos los ejes y la frontera de decisión
+    ax.axhline(0, color='black', lw=0.5)
+    ax.axvline(0, color='black', lw=0.5)
 
     # Graficamos las entradas
     class_0 = x[t.flatten() == 0]
     class_1 = x[t.flatten() == 1]
 
-    ax.scatter(class_0[:, 0], class_0[:, 1], color='red', label='Clase 0')
-    ax.scatter(class_1[:, 0], class_1[:, 1], color='blue', label='Clase 1')
+    ax.scatter(class_0[:, 0], class_0[:, 1], 
+               color='red', marker='s', label='Clase 0')
+    
+    ax.scatter(class_1[:, 0], class_1[:, 1], 
+               color='blue', marker='o', label='Clase 1')
+
+    # Dibujamos la frontera de decisión
+    frontera_decision(ax, w, b, x_range=[-0.5, 1.5])
 
     # Configuramos el espacio de decisión
     ax.set_xlim(-0.5, 1.5)
@@ -31,11 +51,7 @@ def visualizar_iteracion(x,w,b,t,epoca,iteracion,titulo=""):
     # Aplicamos estilo al gráfico
     ax.grid(True, alpha=0.3)
 
-    # Dibujamos los ejes
-    ax.axhline(0, color='black', lw=0.5)
-    ax.axvline(0, color='black', lw=0.5)
-
-    ax.set_title(f'Época {epoca}, Iteración {iteracion}')
+    ax.set_title(f'Época {epoca}, Iteración {iteracion} \n w=[{w[0]:.2f}, {w[1]:.2f}], b={b:.2f}')
     ax.set_xlabel('x1')
     ax.set_ylabel('x2')
     ax.legend()
@@ -113,7 +129,7 @@ def main():
 
     for i in range(1, 7):
         print(f"\n ===  Época {i} ===")
-        w, b, a = perceptron(x,w,b,t_AND,a,epoca=i)
+        w, b, a = perceptron(x,w,b,t_OR,a,epoca=i)
         print(f"Pesos y bias después de la época {i}:")
         imprimir_actual(w,b,a)
 
